78044713af119182030438797d8c1629f23dceaf
Andrey Tsarkov <tm-tsarkov@yandex.ru>
2020-12-12 00:14:38 +0300

    custom editor for savable scriptable objects added, couple bugs fixed

---
+++ Assets/Vis/JustDoThisGoddamnThingUnity/Editor/SavingThisGoddamnScriptableObjectEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Vis.JustSaveThisGoddamnData;
+
+namespace Vis.JustDoThisGoddamnThingUnity
+{
+    /// <summary>
+    /// Just inherit this class for your scriptableObject type to make it goddman savable:)
+    /// </summary>
+    public class SavingThisGoddamnScriptableObjectEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var so = target as ScriptableObject;
+            if (so != default && GUILayout.Button("Save"))
+            {
+                ThisGoddamnScriptableObjectSaverUtility.Save(so);
+            }
+        }
+    }
+}
--- Assets/Vis/JustDoThisGoddamnThingUnity/ThisGoddamnScriptableObjectSaverUtility.cs
+++ Assets/Vis/JustDoThisGoddamnThingUnity/ThisGoddamnScriptableObjectSaverUtility.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 
 namespace Vis.JustSaveThisGoddamnData
 {
+    /*
+     * Saves ScriptableObjects via hacky way of writing new yaml
+     * into .asset file.
+     */
     public static class ThisGoddamnScriptableObjectSaverUtility
     {
         public static void Save<T>(T goddamnScriptableObject) where T : ScriptableObject
         {
+            var goddamnPath = AssetDatabase.GetAssetPath(goddamnScriptableObject);
+            if (string.IsNullOrEmpty(goddamnPath))
+            {
+                return;
+            }
+
             var ourPath = AssetDatabase.GUIDToAssetPath("4c2784811bfb73a488f5c1f3bd4bbbef");
-            var instance = ScriptableObject.CreateInstance<T>();
+            var instance = ScriptableObject.CreateInstance(goddamnScriptableObject.GetType());
             EditorUtility.CopySerialized(goddamnScriptableObject, instance);
-            var goddamnDataPath = AssetDatabase.GetAssetPath(goddamnScriptableObject);
-            var wastePath = Path.Combine(ourPath, Path.GetFileName(goddamnDataPath));
+            var wastePath = Path.Combine(ourPath, Path.GetFileName(goddamnPath));
             AssetDatabase.CreateAsset(instance, wastePath);
             var yamlData = File.ReadAllText(wastePath);
             AssetDatabase.DeleteAsset(wastePath);
             Object.DestroyImmediate(instance);
-            File.WriteAllText(goddamnDataPath, yamlData);
+            File.WriteAllText(goddamnPath, yamlData);
         }
     }
 }
